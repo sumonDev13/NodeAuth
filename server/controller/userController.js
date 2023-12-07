93e605cdc58c512139e25e82e3f75817682318fb
sumonDev13 <sumonmondal1530765@gmail.com>
2023-12-07 20:24:54 +0600

    signUp,signIn

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -18,16 +18,40 @@ export const registerUser = asyncHandler(async (req, res) => {
       email,
       password: await hashPassword(password),
     });
+
+    const token = generateToken(user._id);
   
     if (user) {
       res.status(200).json({
         _id: user._id,
         name: user.name,
         email: user.email,
-        token: generateToken(user._id),
+        token
       });
     } else {
       res.status(400);
       throw new Error("Invalid user data");
+    }
+  });
+
+  export const loginUser = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+  
+    // find user
+  
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.json({ message: "User not exist" });
+    }
+    // verify password
+  
+    const isMatched = await verifyPassword(password, user.password);
+    if (!isMatched) {
+      return res.json({ message: "Invalid login" });
+    } else {
+      return res.json({
+        token: generateToken(user._id),
+        message: "User logged in successfully",
+      });
     }
   });
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { registerUser } from '../controller/userController.js';
+import { loginUser, registerUser } from '../controller/userController.js';
 
 const authRoute = express.Router();
 
 authRoute.post('/signUp',registerUser )
+authRoute.post('/login',loginUser)
 
 export default authRoute;
